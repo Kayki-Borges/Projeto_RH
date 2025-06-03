771842c15012ef64122a7bac76324e39cd919249
Kayki-Borges <144156841+Kayki-Borges@users.noreply.github.com>
2025-06-03 14:38:54 -0300

    projeto_rh

--- candidato/pagina-usuario log.php
+++ candidato/pagina-usuario log.php
@@ -27,23 +27,67 @@ $id_usuario = $usuario['id'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Link Next | Conectando Talentos</title>
      <style>
-      #MudarMod {
-        background: linear-gradient(to right, #ffbb52, white);
-        position: fixed;
-        z-index: 99999999;
-        cursor: pointer;
-        top: 95vh;
-        left: 188vh;
-        border-radius: 5px;
-        border: none;
-        width: 100px;
-        height: 30px;
-        transition: .3s all ease-in-out;
-      }
+      .switch {
+  display: block;
+  --width-of-switch: 3.5em;
+  --height-of-switch: 2em;
+  /* size of sliding icon -- sun and moon */
+  --size-of-icon: 1.4em;
+  /* it is like a inline-padding of switch */
+  --slider-offset: 0.3em;
+  position: fixed;
+  top: 93vh;
+  left: 190vh;
+  z-index: 9999999;
+  width: var(--width-of-switch);
+  height: var(--height-of-switch);
+}
 
-      #MudarMod:hover {
-        transform: scale(1.1);
-      }
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+  border: 2px solid #252525;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #cfdcff;
+  transition: .4s;
+  border-radius: 30px;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: var(--size-of-icon,1.4em);
+  width: var(--size-of-icon,1.4em);
+  border-radius: 20px;
+  left: var(--slider-offset,0.3em);
+  top: 50%;
+  transform: translateY(-50%);
+  background: linear-gradient(40deg,#ff0080,#ff8c00 70%);
+  ;
+ transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #303136;
+}
+
+input:checked + .slider:before {
+  left: calc(100% - (var(--size-of-icon,1.4em) + var(--slider-offset,0.3em)));
+  background: #303136;
+  /* change the value of second inset in box-shadow to change the angle and direction of the moon  */
+  box-shadow: inset -3px -2px 5px -2px #8983f7, inset -10px -4px 0 0 #a3dafb;
+}
 
        body.dark-mode{
         background-color: #252525;
@@ -79,8 +123,10 @@ $id_usuario = $usuario['id'];
     </style>
 </head>
 <body>
-  <button class="mud" id="MudarMod" onclick="mod()">Mudar modo!</button>
-
+  <label class="switch">
+    <input type="checkbox" onclick="mod()">
+    <span class="slider"></span>
+</label>
 <header id="header">
   <nav class="menu">
     <div class="logo">
